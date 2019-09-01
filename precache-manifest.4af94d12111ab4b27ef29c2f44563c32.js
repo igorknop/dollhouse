3d91286803cea53ed0af52313b52e87e15c5cb6d
Igor Knop <igorknop@gmail.com>
2019-08-31 22:21:08 -0300

    Updates

---
+++ precache-manifest.4af94d12111ab4b27ef29c2f44563c32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f6e622b6584f01eaeca6c289472123",
+    "url": "/dollhouse/index.html"
+  },
+  {
+    "revision": "d29a70104138c92e7bf4",
+    "url": "/dollhouse/static/css/main.d233d1a1.chunk.css"
+  },
+  {
+    "revision": "113d05132eea80fae29e",
+    "url": "/dollhouse/static/js/2.214b1311.chunk.js"
+  },
+  {
+    "revision": "d29a70104138c92e7bf4",
+    "url": "/dollhouse/static/js/main.1582cafc.chunk.js"
+  },
+  {
+    "revision": "4ae070aa6b975af6bd9b",
+    "url": "/dollhouse/static/js/runtime~main.55bdf1e3.js"
+  }
+]);
